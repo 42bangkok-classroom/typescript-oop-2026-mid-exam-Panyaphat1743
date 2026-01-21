a8b531518d7d94b9c0b28b5aae7892364c995bbd
Your Name <68011743@kmitl.ac.th>
2026-01-21 15:51:53 +0700

    p05.noex

--- src/practicals/p05/shorten-comment.ts
+++ src/practicals/p05/shorten-comment.ts
@@ -1,5 +1,16 @@
-const comment = "TypeScript is a strongly typed programming language"
+export const comment = "TypeScript is a strongly typed programming language"
+let str:string[] = []
+let res:string =""
 
 function shortenComment(comment: string): string {
-  // Write your code below TESttttt
+  str = comment.split(" ")
+
+  for(let i =0;i<str.length;i++){
+    if(str[i].length>=5 &&str[i].length<=10){
+      res += str[i]
+      res += " "
+    }
+  }
+  return res.trim()
 }
+console.log(shortenComment(comment))
