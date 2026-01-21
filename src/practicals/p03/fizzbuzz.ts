ee88861a5bb1adc7c6279d2e385518d82da7c72c
Your Name <68011743@kmitl.ac.th>
2026-01-21 14:43:58 +0700

    p3

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,14 @@
 // Write your code below
+
+let Inp = Number(process.argv[2])
+for(let i = 1;i<=Inp;i++){
+    if(i%15 == 0){
+        console.log("FizzBuzz")
+    }else if(i%3 == 0){
+        console.log("Fizz")
+    }else if(i%5 == 0){
+        console.log("Buzz")
+    }else{
+        console.log(i)
+    }
+}
