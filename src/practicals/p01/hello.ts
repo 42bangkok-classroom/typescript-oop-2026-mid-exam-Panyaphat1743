6f22f9b81eb4b67fe4ba45763b9aaa6784c82bb3
Your Name <68011743@kmitl.ac.th>
2026-01-21 14:32:57 +0700

    p1 p2

--- src/practicals/p01/hello.ts
+++ src/practicals/p01/hello.ts
@@ -1,2 +1,2 @@
 // Write your code below
-
+console.log("Hello Exam")
--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,1 +1,19 @@
 // Write your code below
+
+let inp = Number(process.argv[2])
+if(Number.isNaN(inp)){
+    console.log("Invalid input")
+}else if(inp<0 || inp>100){
+    console.log("Invalid input")
+}
+if(inp>=80&&inp<=100){
+    console.log("Grade is A")
+}else if(inp>=70&&inp<=79){
+    console.log("Grade is B")
+}else if(inp>=60 && inp<=69){
+    console.log("Grade is C")
+}else if(inp>=50 && inp<=59){
+    console.log("Grade is D")
+}else if(inp>=0&&inp<=49){
+    console.log("Grade is F")
+}
