3c81c9f8dc0c5a8122df9456725ed2005508569d
Your Name <68011743@kmitl.ac.th>
2026-01-21 15:31:04 +0700

    p04

--- src/practicals/p04/number-pattern.ts
+++ src/practicals/p04/number-pattern.ts
@@ -1,1 +1,15 @@
 // Write your code below
+
+let INp = Number(process.argv[2])
+
+
+for(let i = 1;i<=INp;i++){
+    let set = ""
+    for(let y = INp-i;y>0;y--){
+        set += " "
+    }
+    for(let k = i;k>0;k--){
+        set += k
+    }
+    console.log(set)
+}
--- src/practicals/p06/unique.ts
+++ src/practicals/p06/unique.ts
@@ -1,6 +1,22 @@
 function getUniqueNumbers(arr1: number[], arr2: number[]): number[] {
-  // Write your code below
+  let spac:number[] = []
+  for(let i=0;i<arr1.length;i++){
+    for(let y =0;y<arr2.length;y++){
+      if(arr1[i] != arr2[y]){
+        spac = [...spac,...arr1]
+      }
+    }
+  }
+  for(let i=0;i<arr2.length;i++){
+    for(let y =0;y<arr1.length;y++){
+      if(arr1[y] != arr2[i]){
+        spac = [...spac,...arr1]
+      }
+    }
+  }
+  return spac
 }
 
 const arr1 = [1, 2, 3, 4];
 const arr2 = [3, 4, 5, 6];
+console.log(getUniqueNumbers([1, 2, 3, 4],[3, 4, 5, 6]))
